bd03d340e01c98ac71cac46e9bb6ea3c105a9be9
sugamshrestha677 <xresthasugam@gmail.com>
2025-09-06 11:30:40 +0545

    Popular Areas section was made properly.

--- src/sections/PopularAreas.jsx
+++ src/sections/PopularAreas.jsx
@@ -71,10 +71,17 @@ const PopularAreas = () => {
 
         {/* For Bottom one */}
         <div id="bottom" className="w-full grid lg:grid-cols-3 grid-cols-1 lg:justify-center justify-start items-start gap-6">
-          <div data-aos="slide-up" data-aos-delay="200" className="felx justify-center lg:items-center gap-8 w-full">
+          <div data-aos="slide-up" data-aos-delay="200" className="flex justify-center lg:items-center gap-8 w-full">
             <h1 className="text-black font-semibold text-7xl dark:text-white">5K</h1>
-            <h1>ACTIVE <br /><br /> LISTINGS</h1>
-
+            <h1>ACTIVE <br /> LISTINGS</h1>
+          </div>
+          <div data-aos="slide-up" data-aos-delay="200" className="flex justify-center lg:items-center gap-8 w-full">
+            <h1 className="text-black font-semibold text-7xl dark:text-white">5K</h1>
+            <h1>ACTIVE <br /> LISTINGS</h1>
+          </div>
+          <div data-aos="slide-up" data-aos-delay="200" className="flex justify-center lg:items-center gap-8 w-full">
+            <h1 className="text-black font-semibold text-7xl dark:text-white">5K</h1>
+            <h1>ACTIVE <br /> LISTINGS</h1>
           </div>
         </div>
       </section>
